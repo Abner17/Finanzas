bf637183b5fc134f2868ab29ef0bd3b1da7686f1
rocae <ervinroca0909@gmail.com>
2018-10-17 07:21:44 -0600

    Add files via upload

---
+++ Tipo_Cambio, Disponibilidad_Diaria, Autorizacion_Compra Ervin Roca/BancosFinalProt/BancosFinalProt/Frm_DisponibilidadDiaria.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace BancosFinalProt
+{
+    partial class Frm_DisponibilidadDiaria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_DisponibilidadDiaria));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Lbl_Modulo = new System.Windows.Forms.Label();
+            this.Btn_Logo = new System.Windows.Forms.Button();
+            this.Btn_cerrar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDisponibilidad = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.panel1.Controls.Add(this.Lbl_Modulo);
+            this.panel1.Controls.Add(this.Btn_Logo);
+            this.panel1.Controls.Add(this.Btn_cerrar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(933, 42);
+            this.panel1.TabIndex = 1;
+            // 
+            // Lbl_Modulo
+            // 
+            this.Lbl_Modulo.AutoSize = true;
+            this.Lbl_Modulo.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_Modulo.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Modulo.ForeColor = System.Drawing.Color.Black;
+            this.Lbl_Modulo.Location = new System.Drawing.Point(47, 9);
+            this.Lbl_Modulo.Name = "Lbl_Modulo";
+            this.Lbl_Modulo.Size = new System.Drawing.Size(319, 28);
+            this.Lbl_Modulo.TabIndex = 9;
+            this.Lbl_Modulo.Text = "1003_Disponibilidad Diaria";
+            // 
+            // Btn_Logo
+            // 
+            this.Btn_Logo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.Btn_Logo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Logo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.Btn_Logo.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Logo.Image")));
+            this.Btn_Logo.Location = new System.Drawing.Point(0, -2);
+            this.Btn_Logo.Name = "Btn_Logo";
+            this.Btn_Logo.Size = new System.Drawing.Size(41, 41);
+            this.Btn_Logo.TabIndex = 8;
+            this.Btn_Logo.UseVisualStyleBackColor = false;
+            // 
+            // Btn_cerrar
+            // 
+            this.Btn_cerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btn_cerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.Btn_cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_cerrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.Btn_cerrar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_cerrar.Image")));
+            this.Btn_cerrar.Location = new System.Drawing.Point(892, -2);
+            this.Btn_cerrar.Name = "Btn_cerrar";
+            this.Btn_cerrar.Size = new System.Drawing.Size(41, 41);
+            this.Btn_cerrar.TabIndex = 7;
+            this.Btn_cerrar.UseVisualStyleBackColor = false;
+            this.Btn_cerrar.Click += new System.EventHandler(this.Btn_cerrar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(52, 97);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(825, 486);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(253, 610);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Disponibilidad:";
+            // 
+            // txtDisponibilidad
+            // 
+            this.txtDisponibilidad.Location = new System.Drawing.Point(378, 610);
+            this.txtDisponibilidad.Name = "txtDisponibilidad";
+            this.txtDisponibilidad.Size = new System.Drawing.Size(100, 22);
+            this.txtDisponibilidad.TabIndex = 4;
+            this.txtDisponibilidad.TextChanged += new System.EventHandler(this.txtDisponibilidad_TextChanged);
+            // 
+            // Frm_DisponibilidadDiaria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 700);
+            this.Controls.Add(this.txtDisponibilidad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Frm_DisponibilidadDiaria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_EstadosCuenta";
+            this.Load += new System.EventHandler(this.Frm_EstadosCuenta_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Btn_Logo;
+        private System.Windows.Forms.Button Btn_cerrar;
+        private System.Windows.Forms.Label Lbl_Modulo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDisponibilidad;
+    }
+}
---
+++ Tipo_Cambio, Disponibilidad_Diaria, Autorizacion_Compra Ervin Roca/BancosFinalProt/BancosFinalProt/Frm_ProcesoAutorizacionCompra.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BancosFinalProt
+{
+    public partial class Frm_ProcesoAutorizacionCompra : Form
+    {
+        Mdi_Principal principal;
+        public Frm_ProcesoAutorizacionCompra(Mdi_Principal frm)
+        {
+            InitializeComponent();
+            principal = frm;
+        }
+
+        private void Frm_Retiros_Load(object sender, EventArgs e)
+        {
+            DataGridViewTextBoxColumn c1 = new DataGridViewTextBoxColumn();
+            c1.HeaderText = "Codigo";
+            c1.Width = 100;
+            c1.ReadOnly = true;
+
+            DataGridViewTextBoxColumn c2 = new DataGridViewTextBoxColumn();
+            c2.HeaderText = "Nombre";
+            c2.Width = 100;
+            c2.ReadOnly = true;
+
+            DataGridViewTextBoxColumn c3 = new DataGridViewTextBoxColumn();
+            c3.HeaderText = "Cantidad";
+            c3.Width = 100;
+            c3.ReadOnly = true;
+
+            DataGridViewTextBoxColumn c4 = new DataGridViewTextBoxColumn();
+            c4.HeaderText = "Precio";
+            c4.Width = 100;
+            c4.ReadOnly = true;
+
+            DataGridViewTextBoxColumn c5 = new DataGridViewTextBoxColumn();
+            c5.HeaderText = "Total";
+            c5.Width = 100;
+            c5.ReadOnly = true;
+
+            DataGridViewTextBoxColumn c6 = new DataGridViewTextBoxColumn();
+            c6.HeaderText = "Proveedor";
+            c6.Width = 100;
+            c6.ReadOnly = true;
+
+            dataGridView1.Columns.Add(c1);
+            dataGridView1.Columns.Add(c2);
+            dataGridView1.Columns.Add(c3);
+            dataGridView1.Columns.Add(c4);
+            dataGridView1.Columns.Add(c5);
+            dataGridView1.Columns.Add(c6);
+
+
+
+            dataGridView1.Rows.Add();
+            dataGridView1[0, 0].Value = "1";
+            dataGridView1[1, 0].Value = "Laptop";
+            dataGridView1[2, 0].Value = "2";
+            dataGridView1[3, 0].Value = "Q5000";
+            dataGridView1[4, 0].Value = "Q10000";
+            dataGridView1[5, 0].Value = "DELL";
+
+            dataGridView1.Rows.Add();
+            dataGridView1[0, 1].Value = "2";
+            dataGridView1[1, 1].Value = "Impresora";
+            dataGridView1[2, 1].Value = "1";
+            dataGridView1[3, 1].Value = "Q5000";
+            dataGridView1[4, 1].Value = "Q5000";
+            dataGridView1[5, 1].Value = "EPSON";
+
+
+            txt_FechaAutorizacion.Text = "16/10/18";
+            txt_FechaSolicitud.Text = "12/10/18";
+
+        }
+
+        private void Btn_cerrar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            principal.Show();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
